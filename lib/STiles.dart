c1d7e4139a0ee4a4b447654517e08361730c5845
pabushabi <pabushabi@mail.ru>
2021-04-14 16:56:09 +0900

    minor update

--- lib/STiles.dart
+++ lib/STiles.dart
@@ -1,8 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 import 'NotePage.dart';
 import 'Note.dart';
-import 'Utility.dart';
 
 class MyStaggeredTile extends StatefulWidget {
   final Note note;
@@ -24,29 +23,33 @@ class _MyStaggeredTileState extends State<MyStaggeredTile> {
     tileColor = widget.note.noteColor;
     title = widget.note.title;
 
-    return Card(
-      elevation: 2,
-      margin: EdgeInsets.all(2),
-      child: InkWell(
-        splashColor: Color.fromARGB(70, 246, 85, 85),
-        highlightColor: Color.fromARGB(30, 246, 85, 85),
-        onTap: () => _noteTapped(context),
-        child: ListTile(
-          title: title != null
-              ? Text(title,
-                  textScaleFactor: 1.5,
-                  style: TextStyle(fontWeight: FontWeight.w500))
-              : null,
-          subtitle: Text(content, maxLines: 7, textScaleFactor: 1.4),
-        ),
-      ),
-      color: tileColor,
+    return OpenContainer(
+      closedElevation: 0,
+      closedColor: tileColor,
+      closedShape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      transitionType: ContainerTransitionType.fade,
+      openBuilder: (BuildContext context, VoidCallback _) {
+        return NotePage(widget.note);
+      },
+      closedBuilder: (BuildContext _, VoidCallback openContainer) {
+        return Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Color.fromARGB(50, 27, 27, 27)),
+                borderRadius: BorderRadius.circular(10.0)),
+            child: Ink(
+                child: InkWell(
+                    splashColor: Color.fromARGB(70, 246, 85, 85),
+                    highlightColor: Color.fromARGB(30, 246, 85, 85),
+                    onTap: openContainer,
+                    child: ListTile(
+                      title: Text(title,
+                          textScaleFactor: 1.5,
+                          style: TextStyle(fontWeight: FontWeight.w500)),
+                      subtitle:
+                          Text(content, maxLines: 7, textScaleFactor: 1.4),
+                    ))));
+      },
     );
-  }
-
-  void _noteTapped(BuildContext context) {
-    CentralStation.updateNeeded = false;
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => NotePage(widget.note)));
   }
 }
